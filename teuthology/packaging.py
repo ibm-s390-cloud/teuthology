78d0b9aed81e7d5f8999ceac823c5325c9fa3bf6
Aliaksei Makarau <aliaksei.makarau@ibm.com>
2026-07-03 09:43:20 +0200

    return dict(sha1=sha1)

--- teuthology/packaging.py
+++ teuthology/packaging.py
@@ -699,21 +699,24 @@ class GitbuilderProject(object):
                 for n, v in zip(names, vars):
                     log.info('%s: %s' % (n, v))
 
-        if ref:
-            warn('ref')
-            return dict(ref=ref)
-        elif tag:
-            warn('tag')
-            return dict(tag=tag)
-        elif branch:
-            warn('branch')
-            return dict(branch=branch)
-        elif sha1:
-            warn('sha1')
-            return dict(sha1=sha1)
-        else:
-            log.warning("defaulting to main branch")
-            return dict(branch='main')
+        # if ref:
+        #     warn('ref')
+        #     return dict(ref=ref)
+        # elif tag:
+        #     warn('tag')
+        #     return dict(tag=tag)
+        # elif branch:
+        #     warn('branch')
+        #     return dict(branch=branch)
+        # elif sha1:
+        #     warn('sha1')
+        #     return dict(sha1=sha1)
+        # else:
+        #     log.warning("defaulting to main branch")
+        #     return dict(branch='main')
+
+        warn('sha1')
+        return dict(sha1=sha1)
 
     def _get_base_url(self):
         """
